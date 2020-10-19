ada1dc6b579f71b11a9f409b385cf2b2a5e961c2
navanithan <navaneethakumar.m@gmail.com>
2020-10-19 23:19:39 +0900

    added the set interval function

--- js/play.js
+++ js/play.js
@@ -1,29 +1,50 @@
 //import play_data from "data.js";
 var play_data = {
   data: [
-    { name: "Tamil", items: [1, 2, 3, 4] },
+    { name: "Tamil", items: [1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14] },
     { name: "English", items: ["A", "B", "C"] },
-    { name: "", items: ["a", "b", "c"] },
+    { name: "Japanese", items: ["あ", "い", "う","え","お","か","き","く","け","こ",,"さ","し","す","せ","そ","た","ち","つ","て","と","な","に","ぬ","ね"] },
+    { name: "Small English", items: ["a", "b", "c","d","e","f"] },
   ],
 };
 
 var appObj = {
   el: "#app",
-  data: { playData: play_data, selectedPlay: play_data.data[0].name },
+  data: { playData: play_data, selectedPlay: play_data.data[0].name, curDisplayValue:"" },
   vuetify: new Vuetify(),
   methods: {
-    getPlaynames: function () {
+    getPlaynames: function(){
       let nameList = [];
       this.playData.data.forEach((element) => {
         nameList.push(element.name);
       });
       return nameList;
     },
-    displayData: function () {
-      return 10;
-    },
-  },
-};
-var app1 = new Vue(appObj);
+    displayData: function(){
+   //  console.log("on displayData");
+      let curData = this.getPlayData();
 
-//console.log(play_data);
+      let playDataSize = curData.items.length;
+      let dataIndex= Math.floor(Math.random() * Math.floor(playDataSize));
+      console.log("iyt",curData.items[dataIndex]);
+      this.curDisplayValue = curData.items[dataIndex];
+     // console.log(this.curDisplayValue);
+    },
+    getPlayData: function(){
+      //let selectedPlayData = this.play_data
+     // console.log(this.playData);
+     // console.log(this.playData.data);
+      for (let index = 0; index < this.playData.data.length; index++) {
+        const curData = this.playData.data[index];
+        if(curData.name ==this.selectedPlay){
+          return curData;
+          break;
+        }
+      }
+      console.log("Nothing found");
+    }
+  },
+
+};
+var playApp = new Vue(appObj);
+let playTimer = setInterval(playApp.displayData,1000);
